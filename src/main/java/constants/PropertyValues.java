7c82543c161b0c956120055897777a8e5da12e96
Antony Teoh <antony.teoh@gmail.com>
2013-03-27 22:57:28 +0000

    Making class final as it just contain constants

--- src/main/java/constants/PropertyValues.java
+++ src/main/java/constants/PropertyValues.java
@@ -1,10 +1,10 @@
 package constants;
 
-public class PropertyValues {
+public final class PropertyValues {
     public static final String ACTUAL_SCREENSHOT_PATH = "actualScreenshotPath";
     public static final String EXPECTED_SCREENSHOT_PATH = "expectedScreenshotPath";
     public static final String DIFF_SCREENSHOT_PATH = "diffScreenshotPath";
-    public static final String PATH_TO_IM_BINARY = "pathToImageMagickBinary";
+    public static final String PATH_TO_IM_COMPARE_BINARY = "pathToImageMagickCompareBinary";
     public static final String RESULTS_FILE_PATH = "resultsFilePath";
     public static final String RESULTS_FILE_NAME = "resultsFileName";
     public static final String RESULTS_FILE_TYPE = "resultsFileType";
